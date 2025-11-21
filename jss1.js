b89b96d705a5f6808891355a4a63ac350cf938c8
Yehor Bykov <ebikov2ebikov269@gmail.com>
2025-11-21 08:18:36 +0100

    Add files via upload

---
+++ jss1.js
@@ -0,0 +1,25 @@
+function obliczPole() {
+    let a = parseFloat(document.getElementById("a").value);
+    let b = parseFloat(document.getElementById("b").value);
+    let c = parseFloat(document.getElementById("c").value);
+
+    let wynik = document.getElementById("wynik");
+
+    if (a + b <= c  b + c <= a) {
+        wynik.textContent = "Z podanych boków nie sposób utworzyć trójkąta!";
+        return; 
+    }
+
+    let s = (a + b + c) / 2;
+
+   
+    let pole = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+    wynik.textContent = "Pole trójkąta wynosi: " + pole.toFixed(4);
+    
+}
+
+let obliczButton = document.getElementById('obliczButton');
+if (obliczButton) {
+    obliczButton.addEventListener('click', obliczPole);
+}
